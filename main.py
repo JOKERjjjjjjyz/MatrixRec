9c1e1e0d1ed681054ba41156146450b4df4f3497
jyz <genius_j@sjtu.eud.cn>
2023-08-29 19:07:32 +0800

    let's go 3

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ if __name__ == '__main__':
     elif world.dataset == 'lastfm':
         dataset = dataloader.Loader(path="./data")
 
-    core = int(world.CORES)
-    print(core)
     graph,norm_graph = dataset.getSparseGraph()
     C=norm_graph
     print(graph)
@@ -37,12 +35,12 @@ if __name__ == '__main__':
     for idx, user in enumerate(dataset.test):
         testarray[idx] = dataset.test[user]
     print(C_sum.shape)
-    vector_propagate = Mrow(C_sum,M).dot(uservector)
-    print("topK here")
-    recommendList, recommend_vector = topK(uservector, rowM(vector_propagate,M), M, N, 20)
-    count = evaluate(recommendList, testarray)
-    recall = count / dataset.testDataSize
-    print("sum ver:epoch:",1," recall:", recall)
+    # vector_propagate = Mrow(C_sum,M).dot(uservector)
+    # print("topK here")
+    # recommendList, recommend_vector = topK(uservector, rowM(vector_propagate,M), M, N, 20)
+    # count = evaluate(recommendList, testarray)
+    # recall = count / dataset.testDataSize
+    # print("sum ver:epoch:",1," recall:", recall)
     for i in range(2,K+1):
         print("epoch",i,"start here")
         C = C.dot(norm_graph) * alpha * math.pow(1-alpha,i-1)
@@ -63,8 +61,8 @@ if __name__ == '__main__':
         print("not sum ver:epoch:",i," recall:", recall)
         vector_propagate = C_user_sum.dot(uservector)
         recommendList, recommend_vector = topK(uservector, rowM(vector_propagate,M), M, N, 20)
-        # recommend_vector_csr = csr_matrix(recommend_vector)
-        # sp.save_npz(dataset.path + '/recommend_vector_{i}.npz', recommend_vector_csr)
+        recommend_vector_csr = csr_matrix(recommend_vector)
+        sp.save_npz(dataset.path + '/recommend_vector_{i}.npz', recommend_vector_csr)
         count = evaluate(recommendList, testarray)
         recall = count / dataset.testDataSize
         print("sum ver:epoch:",i," recall:", recall)
@@ -117,36 +115,3 @@ if __name__ == '__main__':
     # recall = count / dataset.testDataSize
     # print ("Final recall:",recall)
     # # dense_array = dataset.UserItemNet.toarray()
-
-    # 将原始 stdout 保存到变量
-    # B_cpu = B.to('cpu')
-    C = C.toarray()
-    # print("Transposed UserItemNet * UserItemNet:")
-    # print(C)
-    original_stdout = sys.stdout
-
-    # 打开一个文件来替代 stdout
-    with open('recall_output.txt', 'w') as f:
-        # 重定向 stdout 到文件
-        sys.stdout = f
-        print("Final matrix:",C )
-        # 现在所有的 print 输出都会写入到文件中
-        # with np.printoptions(threshold=np.inf):
-        #     print("0:",vector_array[0])
-        #     print("1:",vector_array[1])
-        # print("users:",dataset.n_users)
-        # print("items:",dataset.m_items)
-    # 恢复原始的 stdout
-    sys.stdout = original_stdout
-    # user_item_net_dense = torch.tensor(UserItemNet, dtype=torch.float32)
-    # file_path = dataset.path + "/saving_files"
-    # UserItemNet_gpu = torch.sparse_coo_tensor(
-    #     indices=torch.tensor(graph.nonzero(), dtype=torch.int64).to('cuda'),
-    #     values=torch.tensor(graph.data, dtype=torch.float32).to('cuda'),
-    #     size=graph.shape
-    # )
-    #
-    # # 假设 user_item_net_dense 是一个稠密张量
-    # user_item_net_transposed = user_item_net_dense.t()
-    # user_item_net_transposed_gpu = user_item_net_transposed.to('cuda')
-    # B = torch.sparse.mm(UserItemNet_gpu, user_item_net_transposed_gpu)
--- train.py
+++ train.py
@@ -4,102 +4,22 @@ import math
 import time
 import scipy.sparse
 import multiprocessing
-def randomwalk(length,graph,start_node):
-    current_node = start_node
-    for step in range(length):
-        # 获取当前节点的邻居节点
-        neighbors = graph[current_node].indices
-        if len(neighbors) == 0:
-            # 当前节点没有邻居，随机游走结束
-            break
-        # 随机选择一个邻居作为下一步的节点
-        next_node = random.choice(neighbors)
-        # 更新当前节点为下一步的节点
-        current_node = next_node
-    radio = 1/length
-    return current_node,radio
-
 def rowM(matrix,M):
     B = matrix[:M]
     return B
 def Mrow(matrix,M):
-    # 假设 A 是一个 N*N 的 CSR 矩阵，M 是你想要提取的行数
-    # 提取前 M 行的数据
-    # N = matrix.shape[0]
-    # print(N)
-    # print(matrix.indptr,matrix.data,matrix.indices)
-    # B_data = matrix.data[:M]
-    # B_indices = matrix.indices[:M]
-    # B_indptr = matrix.indptr[:M + 1]
-    #
-    # print(B_indices,B_indptr)
-    # # 构建 CSR 矩阵 B
-    # B = scipy.sparse.csr_matrix((B_data, B_indices, B_indptr), shape=(M,N))
     B = matrix[:M]
     B = B.transpose()
     print (B.shape)
     return B
 
-def propagate(k,graph,vector_origin,M,N,KsampleNum):
-    user_list = [i for i in range(M)]
-    vector = np.zeros((M + N, N))
-    for user in user_list:
-        start_time=time.time()
-        for j in range(KsampleNum):
-            print("Training:Epoch",k,",(user,j):(",user,",",j,")")
-            targetNode,radio = randomwalk(k, graph, user)
-            vector[targetNode] += radio*vector_origin[user]*0.001
-        end_time = time.time()
-        use_time = end_time - start_time
-        print("user",user,"'s time:",use_time,"s")
-    return vector
-
-def Klayer_sampleNum(k,epsilon,delta,M,index):
-    # return N: sample number for k
-    N = 1/(2*epsilon*epsilon)*math.log(2*M/delta)*M*math.pow(k,index)
-    return int(N)+1
-# def topK_worker(chunk, vector_origin, vector_propagate, N,k):
-#     recommendList = []
-#     recommend_vector = np.zeros(N)
-#     vector_propagate_copy = vector_propagate.copy()
-#     vector_origin_copy = vector_origin.copy()
-#     # 在复制的矩阵上执行操作
-#     vector = vector_propagate_copy - 10000 * vector_origin_copy
-#     for user in chunk:
-#         sorted_indices = np.argsort(vector[user])
-#         topk_indices = sorted_indices[-k:]
-#         for idx in topk_indices:
-#             recommend_vector[user,idx] = 1
-#             recommendList.append((user, idx))
-#     return recommendList, recommend_vector
-#
-# def topK_parallel(vector_origin, vector_propagate, M, N, k, num_cores):
-#     recommendList = []
-#     recommend_vector = [np.zeros(N) for _ in range(M)]
-#     print(type(vector_origin),type(vector_propagate))
-#     pool = multiprocessing.Pool(processes=num_cores)
-#     chunk_size = M // num_cores
-#     chunks = [list(range(i * chunk_size, (i + 1) * chunk_size)) for i in range(num_cores)]
-#     results = []
-#     for chunk in chunks:
-#         results.append(pool.apply_async(topK_worker, (chunk, vector_origin, vector_propagate,N, k)))
-#     pool.close()
-#     pool.join()
-#     for result in results:
-#         chunk_recommendList, chunk_recommend_vector = result.get()
-#         recommendList.extend(chunk_recommendList)
-#         for user in chunk:
-#             recommend_vector[user] = chunk_recommend_vector[user]
-#     return recommendList, recommend_vector
-
 def topK(vector_origin,vector_propagate,M,N,k):
     recommendList = []
     recommend_vector = [np.zeros(N) for _ in range(M)]
-    vector = vector_propagate - 10000*vector_origin
+    vector = vector_propagate - 1000*vector_origin
     print(type(vector_origin),vector_origin.shape,type(vector_propagate),vector_propagate.shape)
     for user in range(M):
         print("topK of user",user)
-        # recommend_vector = vector_propagate - 10000*vector_origin
         sorted_indices = np.argsort(vector[user])
         topk_indices = sorted_indices[-k:]
         for idx in topk_indices:
@@ -124,13 +44,3 @@ def evaluate(recommendList, test):
                 break
         print("Evaluating:",count2,"/",RecLenth)
     return count
-
-# def main(graph,vector_origin)
-#     # M:user number; N: item number
-#     # vector_origin: M*N;  vector_propagate: (M+N)*N
-#     vector_propagate = [0]
-#     for i:1 to K do:
-#         vector_propagate = propagate(i,graph,vector_origin,vector_propagate)
-#     topK(vector_origin,vector_propagate)
-#     evaluate(recommendList , test)
-#     return 0
